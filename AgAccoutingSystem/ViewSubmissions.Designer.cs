9c22f4a567d8ad7bce8826d7db81cd4020083eef
John Adams <jcadams997@ksu.edu>
2021-04-13 11:55:33 -0500

    Button adds to view Sub form

--- AgAccoutingSystem/ViewSubmissions.Designer.cs
+++ AgAccoutingSystem/ViewSubmissions.Designer.cs
@@ -29,12 +29,44 @@ namespace AgAccoutingSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ConfirmBttn = new System.Windows.Forms.Button();
+            this.ExitBttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConfirmBttn
+            // 
+            this.ConfirmBttn.Location = new System.Drawing.Point(276, 373);
+            this.ConfirmBttn.Name = "ConfirmBttn";
+            this.ConfirmBttn.Size = new System.Drawing.Size(75, 23);
+            this.ConfirmBttn.TabIndex = 0;
+            this.ConfirmBttn.Text = "Confirm";
+            this.ConfirmBttn.UseVisualStyleBackColor = true;
+            // 
+            // ExitBttn
+            // 
+            this.ExitBttn.Location = new System.Drawing.Point(416, 373);
+            this.ExitBttn.Name = "ExitBttn";
+            this.ExitBttn.Size = new System.Drawing.Size(75, 23);
+            this.ExitBttn.TabIndex = 1;
+            this.ExitBttn.Text = "Exit";
+            this.ExitBttn.UseVisualStyleBackColor = true;
+            // 
+            // ViewSubmissions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExitBttn);
+            this.Controls.Add(this.ConfirmBttn);
+            this.Name = "ViewSubmissions";
             this.Text = "ViewSubmissions";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ConfirmBttn;
+        private System.Windows.Forms.Button ExitBttn;
     }
 }
