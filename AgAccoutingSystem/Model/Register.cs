931e4356aca4e8189fd4d99531c36de9be679c0f
John Adams <jcadams997@ksu.edu>
2021-04-15 15:44:11 -0500

    Most up to date

--- AgAccoutingSystem/Model/Register.cs
+++ AgAccoutingSystem/Model/Register.cs
@@ -8,5 +8,26 @@ namespace AgAccoutingSystem
 {
     class Register
     {
+        private int transactionID;
+        public int TransactionID
+        {
+            get { return transactionID; }
+            set { transactionID = value;  }
+        }
+
+        private int code;
+        public int Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        private int accountID;
+        public int AccountID
+        {
+            get { return accountID; }
+            set { accountID = value; }
+        }
+
     }
 }
